b51a7968f37443ed2c9bd83a49995f1759eae010
MikaelFeher <mikael.feher@gmail.com>
2016-07-09 13:06:32 +0200

    added middleman blog gem and configuration

--- config.rb
+++ config.rb
@@ -19,6 +19,11 @@ page '/*.txt', layout: false
 # proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
 #  which_fake_page: "Rendering a fake page with a local variable" }
 
+# Blog options
+activate :blog do |blog|
+  # set options on blog
+end
+
 # General configuration
 set :relative_links, true
 activate :directory_indexes
