15b7123fa85e61d3bc2966180b776f39203510e0
MikaelFeher <mikael.feher@gmail.com>
2016-07-10 16:59:17 +0200

    added a new source folder for blog articles and added links for blog and home pages in the header and footer

--- config.rb
+++ config.rb
@@ -9,6 +9,7 @@ require 'lib/icon_helper'
 #
 # With no layout
 page '/*.xml', layout: false
+page '/feed.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
 
@@ -16,7 +17,6 @@ page '/*.txt', layout: false
 set :relative_links, true
 activate :directory_indexes
 activate :i18n, mount_at_root: :false
-page "/feed.xml", layout: false
 
 ###
 # Helpers
@@ -25,11 +25,11 @@ helpers IconHelper
 
 activate :blog do |blog|
   # This will add a prefix to all links, template references and source paths
-  # blog.prefix = "blog"
+  # blog.prefix = 'blog'
 
-  # blog.permalink = "{year}/{month}/{day}/{title}.html"
+  blog.permalink = ':title.html'
   # Matcher for blog source files
-  # blog.sources = "{year}-{month}-{day}-{title}.html"
+  blog.sources = 'blog_articles/:title.html.yml'
   # blog.taglink = "tags/{tag}.html"
   # blog.layout = "layout"
   # blog.summary_separator = /(READMORE)/
@@ -37,15 +37,15 @@ activate :blog do |blog|
   # blog.year_link = "{year}.html"
   # blog.month_link = "{year}/{month}.html"
   # blog.day_link = "{year}/{month}/{day}.html"
-  # blog.default_extension = ".markdown"
+  # blog.default_extension = '.yml'
 
   blog.tag_template = 'tag.html'
   blog.calendar_template = 'calendar.html'
 
   # Enable pagination
-  # blog.paginate = true
-  # blog.per_page = 10
-  # blog.page_link = "page/{num}"
+  blog.paginate = true
+  blog.per_page = 10
+  blog.page_link = 'page/{num}'
 end
 
 # Deployment configuration
